3308455aa156e2304c2b68a9a7fc12c63f1beac4
gmthj <66828440+gmthj@users.noreply.github.com>
2021-01-22 02:13:30 +0800

    implementaion of Person Class

---
+++ CovidApp/Person.cs
@@ -0,0 +1,69 @@
+﻿//============================================================
+// Student Number : S10203190
+// Student Name : Tan Hiang Joon Gabriel
+// Module Group : T07
+//============================================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidApp
+{
+    abstract class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private List<SafeEntry> safeEntryList;
+
+        public List<SafeEntry> SafeEntryList
+        {
+            get { return safeEntryList; }
+            set { safeEntryList = value; }
+        }
+
+        private List<TravelEntry> travelEntryList;
+
+        public List<TravelEntry> TravelEntryList
+        {
+            get { return travelEntryList; }
+            set { travelEntryList = value; }
+        }
+
+        public Person()
+        {
+            SafeEntryList = new List<SafeEntry>();
+            TravelEntryList = new List<TravelEntry>();
+        }
+
+        public Person(string n)
+        {
+            Name = n;
+            SafeEntryList = new List<SafeEntry>();
+            TravelEntryList = new List<TravelEntry>();
+        }
+
+        public void AddTravelEntry(TravelEntry travelEntry)
+        {
+            TravelEntryList.Add(travelEntry);
+        }
+
+        public void AddSafeEntry(SafeEntry safeEntry)
+        {
+            SafeEntryList.Add(safeEntry);
+        }
+
+        public abstract double CalculateSHNCharges();
+
+        public override string ToString()
+        {
+            return "Name: " + Name;
+        }
+    }
+}
--- CovidApp/Program.cs
+++ CovidApp/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//============================================================
+// Student Number : S10203190, S10203166
+// Student Name : Tan Hiang Joon Gabriel, Marc Lim Liang Kiat
+// Module Group : T07
+//============================================================
+
+using System;
 
 namespace CovidApp
 {
