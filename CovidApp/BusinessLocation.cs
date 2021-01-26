01ccac9cc03ef0c39e55d8facbfa966ad86933d2
Marc Lim Liang Kiat <s10203166@connect.np.edu.sg>
2021-01-26 23:38:31 +0800

    Fully Implemented the BusinessLocation class and implemented the Resident class up to the section which requires other classes to be implemented first

---
+++ CovidApp/BusinessLocation.cs
@@ -0,0 +1,71 @@
+﻿//============================================================
+// Student Number : S10203166
+// Student Name : Marc Lim Liang Kiat
+// Module Group : T07
+//============================================================
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidApp
+{
+    class BusinessLocation
+    {
+        private string businessName;
+
+        public string BusinessName
+        {
+            get { return businessName; }
+            set { businessName = value; }
+        }
+
+        private string branchCode;
+
+        public string BranchCode
+        {
+            get { return branchCode; }
+            set { branchCode = value; }
+        }
+
+        private int maximumCapacity;
+
+        public int MaximumCapacity
+        {
+            get { return maximumCapacity; }
+            set { maximumCapacity = value; }
+        }
+
+        private int visitorsNow;
+
+        public int VisitorsNow
+        {
+            get { return visitorsNow; }
+            set { visitorsNow = value; }
+        }
+
+        public BusinessLocation() { }
+
+        public BusinessLocation(string name, string branchCode, int maximumCapacity)
+        {
+            BusinessName = name;
+            BranchCode = branchCode;
+            MaximumCapacity = maximumCapacity;
+            VisitorsNow = 0;
+        }
+
+        public bool IsFull()
+        {
+            if (VisitorsNow == MaximumCapacity) // "==" is used instead of ">=" as there should not be a case where VisitorsNow > MaximumCapacity if checks are done correctly in SafeEntry class
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public override string ToString()
+        {
+            return "Business Name: " + BusinessName + "/t Branch Code: " + BranchCode + "/tMaximum Capacity: " + MaximumCapacity + "/tVisitors Now: " + VisitorsNow;
+        }
+    }
+}
---
+++ CovidApp/Resident.cs
@@ -0,0 +1,46 @@
+﻿//============================================================
+// Student Number : S10203166
+// Student Name : Marc Lim Liang Kiat
+// Module Group : T07
+//============================================================
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidApp
+{
+    class Resident : Person
+    {
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private DateTime lastLeftCountry;
+
+        public DateTime LastLeftCountry
+        {
+            get { return lastLeftCountry; }
+            set { lastLeftCountry = value; }
+        }
+
+        private TraceTogetherToken token;
+
+        public TraceTogetherToken Token
+        {
+            get { return token; }
+            set { token = value; }
+        }
+
+        public Resident(string name, string address, DateTime lastLeftCountry): base(name)
+        {
+            Address = address;
+            LastLeftCountry = lastLeftCountry;
+        }
+
+
+    }
+}
