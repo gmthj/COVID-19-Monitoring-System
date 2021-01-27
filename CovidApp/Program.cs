eebcefa65fa0233735fd5a6e1111abfd912e3aa7
gmthj <66828440+gmthj@users.noreply.github.com>
2021-01-28 02:56:51 +0800

    work on Program class - Basic feature 1 - added LoadPersonData method to load person.csv into a list - working on LoadVisitor to add visitors to personList

--- CovidApp/Program.cs
+++ CovidApp/Program.cs
@@ -5,6 +5,8 @@
 //============================================================
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CovidApp
 {
@@ -12,9 +14,56 @@ namespace CovidApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //hello ooga booga test
-            //hello booya nikka wheres my mony
+            //Initialisation
+            List<string[]> personDataList = new List<string[]>();
+            List<Person> personList = new List<Person>();
+
+
+
+            //BASIC FEATURES
+
+            //1) Load Person and Business Location Data
+            LoadPersonData(personDataList);
+            
+
+
+            //random testing
+            foreach (string[] arr in personDataList)
+            {
+                foreach (string data in arr)
+                {
+                    Console.Write(data + "\t");
+                }
+                Console.WriteLine("");
+            }
         }
+
+        //1) Load Person Data
+        static void LoadPersonData(List<string[]> personDataList)
+        {
+            using (StreamReader sr = new StreamReader("csv files/Person.csv"))
+            {
+                string line = sr.ReadLine();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] splitLine = line.Split(",");
+                    personDataList.Add(splitLine);
+                }
+            }
+        }
+
+        static void LoadVisitor(List<string[]> personDataList, List<Person> personList) //wip
+        {
+            foreach (string[] personArray in personDataList)
+            {
+                if (personArray[0] == "visitor")
+                {
+                    personList.Add(new Visitor(personArray[1], personArray[4], personArray[5]));
+                }
+            }
+        }
+
+        //1) Load Business Location Data
+
     }
 }
